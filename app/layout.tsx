8ba9a49141e233c485adc91382587b8db3a09cb8
henophilia <tech@henophilia.org>
2025-06-08 19:47:57 +0200

    fix: layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,38 @@
-import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
-import "./globals.css"
-import { redirect } from "next/navigation"
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { redirect } from "next/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-})
+});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-})
+});
 
 export const metadata: Metadata = {
   title: "Hermes' Loom",
   description: "Hermes' Loom",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   // Unconditionally redirect to henophilia.art
-  redirect("https://henophilia.art")
+  redirect("https://henophilia.art");
 
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}></body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
